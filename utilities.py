9d590c2b4c31368ffab562fc026134ba71b6ccf7
Meha Sadasivam <ms5941@gsb.columbia.edu>
2021-04-10 01:54:10 -0400

    Adding some more potentially recyclable code from big data - placeholders

--- utilities.py
+++ utilities.py
@@ -38,3 +38,67 @@ def filtered_article_count(data, language='en', subjects=None, subject_filter_ty
             if eval(subject_filter_type)(subject in art['data']['subjects'] for subject in subjects):
                 count +=1
     return count
+
+
+# PLACEHOLDERS: EVERYTHING COPIED FROM BIGDATA HW FROM LAST YEAR
+
+# TODO: Check if we need all the imports!
+# And is there another document term matrix api we can use? Not sure if we need all this (stemming probably not)
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Use CountVectorizer's analyzer to process the articles
+analyzer = CountVectorizer(stop_words='english').build_analyzer()
+
+# The default analyzer doesn't stem, so we do some post-processing
+def own_analyzer(docstr):
+    return [stemmer.stem(wrd) for wrd in analyzer(docstr)]
+
+# The countvectorizer needed while building Python's DTM 
+cv = CountVectorizer(analyzer=own_analyzer)
+
+            
+def get_dtm_sparse_matrix(articles):
+    dtm_raw = cv.fit_transform([art['data']['body'] for art in articles if art['data']['body'] != ''])
+    return dtm_raw
+
+
+def get_required_word_idx_freq_list(dtm_sparse):
+    freq = dtm_sparse.sum(axis=0)  # sum along the columns --> returns a [1 x num cols] matrix
+    # The order of this is the same as in the cv.vocabulary_ lookup table
+    # Let's now associate the word label with its position in the dtm and its frequency count
+    named_freq = [(wrd, idx, freq[0, idx]) for wrd, idx in cv.vocabulary_.items()]
+
+    # Sort from highest to lowest frequency word (the order is preserved throughout)
+    named_freq = sorted(named_freq, key = lambda xx: xx[2], reverse=True)
+    # We only want the top 101-2100 frequent words 
+    reqd = named_freq[101:2101]
+    return reqd
+
+
+def get_cosine_similarity_matrix(dtm_sparse, reqd_word_idx_list):    
+    reqd_idx = [el[1] for el in reqd_word_idx_list]
+
+    # get the m and c matrixes
+    mm = dtm_sparse[:, reqd_idx]
+    c1 = (mm.transpose()*mm).todense()  ## convert to dense matrix
+
+    # calculate the cosine similarity matrix
+    d1 = np.sqrt(np.diag(c1))
+    d1 = np.reshape(d1, (len(d1), 1))  ## convert array to a Nx1 matrix
+    d2 = d1.transpose() * d1
+    cc = np.multiply(c1,1/d2)  ## this is element by element multiplication
+    print('Shape of cosine similarity matrix:', cc.shape)
+    return cc
+
+
+def get_most_similar_words(cc, reqd_word_idx_freq_list, word_list, number=10):
+    words_ordered = [wrd for wrd,_,_ in reqd_word_idx_freq_list] 
+    for tgt_wrd in word_list:
+        # Find index of the target word
+        wrd_idx = words_ordered.index(tgt_wrd)                                      
+        # Associate words with cosine similarity
+        words_dict = {(wrd, cc[ii, wrd_idx]) for ii, wrd in enumerate(words_ordered)}  
+        # Sort by cosine similarity measure
+        words_dict = sorted(words_dict, key=lambda xx:xx[1],reverse=True)
+        print(tgt_wrd, 'most similar:', words_dict[:number], end='\n\n')
