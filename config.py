a82e70c5986848e686e310258beed29c1a44e5c9
Espino Robles <ee2472@researchgrid.gsb.columbia.edu>
2021-04-20 15:18:12 -0400

    Added 2 filters of the reuters thing

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ OUTPUT_PATH = '/work/ms5941/NLP/Outputs'
 
 # We only want US articles
 SUBJECTS = 'N2:US'
+
+#This are the topic codes that Thomson Reuters already has
+CODE_WORDS = ['INFL','GDP','']
