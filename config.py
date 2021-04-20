edd0298129e29887443afd9f5e26e3964c609e34
Meha Sadasivam <ms5941@gsb.columbia.edu>
2021-04-20 15:26:50 -0400

    Minor config change and added notebook to process gensim word similarity

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ TEMP_PATH = '/work/ms5941/NLP/Temp'
 OUTPUT_PATH = '/work/ms5941/NLP/Outputs'
 
 # We only want US articles
-SUBJECTS = 'N2:US'
+SUBJECTS = ['N2:US']
 
 #This are the topic codes that Thomson Reuters already has
 CODE_WORDS = ['INFL','GDP','']
+
