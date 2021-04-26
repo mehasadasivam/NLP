da9edcbd403a3bb4e46b7a3d67d46df21115f890
Meha Sadasivam <ms5941@gsb.columbia.edu>
2021-04-26 13:08:10 -0400

    Restructuring folders and adding the final corpus and dicts to repo

---
+++ Similarity Model/GDP_tokenizing.py
@@ -0,0 +1,24 @@
+#!/apps/anaconda3/bin/python
+
+import os
+from config import *
+import utilities as utils
+import json
+import importlib
+import pandas as pd
+
+def main():
+    increment = os.environ['SGE_TASK_ID']
+    year = str(START_YEAR - 1 + int(increment)) # Subtract 1 since the task ids start from 1
+    
+    articles = utils.get_tokenized_articles(year, 'GDP')
+    print('Articles tokenized!')
+    
+    tok_articles_ev = utils.get_tokenized_articles_within_effective_vocab(articles)
+    print('Tokenized articles with effective vocab done!')
+
+    with open(TEMP_PATH + '/GDP/GDP_Articles_Tokenized_%s.json' % year, 'w') as f:
+        json.dump(tok_articles_ev, f)
+
+if __name__ == '__main__':
+    main()
---
+++ Similarity Model/similaritymodel.py
@@ -0,0 +1,54 @@
+#!/apps/anaconda3/bin/python
+
+import os
+from config import *
+import utilities as utils
+import json
+import importlib
+import nltk
+stopwords_list = nltk.corpus.stopwords.words('english')
+import re
+from gensim.models import Word2Vec
+
+def main():
+    year_files_dict = utils.get_all_filenames()
+    increment = os.environ['SGE_TASK_ID']
+    year = str(START_YEAR - 1 + int(increment)) # Subtract 1 since the task ids start from 1
+    
+    all_filenames = year_files_dict[year]
+
+    tokenized_sentences = list()
+
+    for fname in all_filenames:
+
+        data = utils.load_data(fname)
+        count = 0
+        for art in data['Items']:
+            text = ''
+            if art['data']['language'] == 'en':
+                if any(subject in art['data']['subjects'] for subject in [US_CODE]):
+                    text = art['data']['body']
+
+            if not text:
+                continue
+            count += 1
+            text = nltk.sent_tokenize(text)
+            text = [sentence.replace('\n', ' ') for sentence in text]
+            doc_words = []
+            for sentence in text:
+                tokenized_sent = list()-
+                # Only keeping alphabets and replacing everything else with spaces
+                sentence = re.sub(r'[^A-Za-z]+', ' ', sentence)
+                sentence = sentence.lower()
+                sent_words = nltk.word_tokenize(sentence)
+                sent_words = [word for word in sent_words if ((len(word) > 1) and (len(word) < 20))]
+                sent_words = [word for word in sent_words if word not in stopwords_list]
+                tokenized_sentences.append(sent_words)
+        print(fname[-15:-8], 'Done! Article Count:', count, 'Creating word2vec model now.')
+
+    # Build word2vec model and store in temp location
+    model = Word2Vec(tokenized_sentences)
+    model.save(TEMP_PATH + '/' + 'all_reuters_news_articles_%s.w2v' % (year,))
+
+if __name__ == '__main__':
+    main()
